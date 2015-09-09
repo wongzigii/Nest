7acfefe79262f953a1cf8da015493b04d81745b5
WeZZard <wezzardlau@gmail.com>
2015-09-10 07:32:39 +0800

    Make NSIndexPath to conform to Comparable

--- Nest/Extensions/NSIndexPath+Comparable.swift
+++ Nest/Extensions/NSIndexPath+Comparable.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+extension NSIndexPath: Comparable {
+    
+}
+
+public func ==(lhs: NSIndexPath, rhs: NSIndexPath) -> Bool {
+    var lhsIndices = [Int]()
+    var rhsIndices = [Int]()
+    lhs.getIndexes(&lhsIndices)
+    rhs.getIndexes(&rhsIndices)
+    return lhsIndices == rhsIndices
+}
+
+public func <(lhs: NSIndexPath, rhs: NSIndexPath) -> Bool {
+    var lhsIndices = [Int]()
+    var rhsIndices = [Int]()
+    lhs.getIndexes(&lhsIndices)
+    rhs.getIndexes(&rhsIndices)
+    
+    repeat {
+        let lhsFirst = lhsIndices.removeFirst()
+        let rhsFirst = rhsIndices.removeFirst()
+        
+        if lhsFirst == rhsFirst {
+            continue
+        } else {
+            return lhsFirst < rhsFirst
+        }
+    } while lhsIndices.count > 0 && rhsIndices.count > 0
+    
+    return lhsIndices.count < rhsIndices.count
+}
