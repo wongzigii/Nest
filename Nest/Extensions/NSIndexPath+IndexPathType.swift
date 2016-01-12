ecff949a837d7575813d35c5abc5b41c53e53170
WeZZard <wezzardlau@gmail.com>
2016-01-12 08:08:32 +0800

    Adopt SwiftExt's changes to NSIndexPath.

--- Nest/Extensions/NSIndexPath+IndexPathType.swift
+++ Nest/Extensions/NSIndexPath+IndexPathType.swift
@@ -9,45 +9,71 @@
 import SwiftExt
 import Foundation
 
-extension NSIndexPath: HirarchicalIndexType {
+extension NSIndexPath: HierarchicalIndexType {
     public typealias Index = Int
     
     public var indices: [Int] {
-        var indices = [Int]()
-        for position in 0..<length {
-            indices.append(indexAtPosition(position))
+        let indices = UnsafeMutablePointer<Int>.alloc(length)
+        getIndexes(indices)
+        return Array<Int>(head: indices, length: length)
+    }
+    
+    public func parent() -> Self {
+        guard length > 0 else {
+            fatalError("Cannot get parent from a zero-length index path.")
         }
-        return indices
+        
+        let parentLength = max(0, length - 1)
+        let indices = UnsafeMutablePointer<Int>.alloc(parentLength)
+        getIndexes(indices, range: NSRange(location: 0, length: parentLength))
+        
+        return self.dynamicType.init(indexes: indices, length: parentLength)
+    }
+    
+    public func childWith(index: Index) -> Self {
+        
+        let childLength = max(0, length + 1)
+        let indices = UnsafeMutablePointer<Int>.alloc(childLength)
+        getIndexes(indices)
+        indices[max(0, childLength - 1)] = index
+        
+        return self.dynamicType.init(indexes: indices, length: childLength)
     }
     
     public func predecessor() -> Self {
-        guard let lastIndex = self.indices.last else {
-            return self
-        }
+        guard length > 0 else { return self }
         
-        var indices = self.indices
-        indices.removeLast()
-        indices += lastIndex.predecessor()
+        let indices = UnsafeMutablePointer<Int>.alloc(length)
+        getIndexes(indices)
+        let last = max(0, length - 1)
+        let lastIndex = indices[last]
+        indices[last] = lastIndex - 1
         
-        return self.dynamicType.init(indexes: indices, length: indices.count)
+        return self.dynamicType.init(indexes: indices, length: length)
     }
     
     public func successor() -> Self {
-        guard let lastIndex = self.indices.last else {
-            return self
-        }
+        guard length > 0 else { return self }
         
-        var indices = self.indices
-        indices.removeLast()
-        indices += lastIndex.successor()
+        let indices = UnsafeMutablePointer<Int>.alloc(length)
+        getIndexes(indices)
+        let last = max(0, length - 1)
+        let lastIndex = indices[last]
+        indices[last] = lastIndex + 1
         
-        return self.dynamicType.init(indexes: indices, length: indices.count)
+        return self.dynamicType.init(indexes: indices, length: length)
     }
     
     public func umbrellas(indexPath: NSIndexPath) -> Bool {
-        if length < indexPath.length {
+        if length < indexPath.length && length > 0 {
             return indices[0] == indexPath.indices[0]
         }
         return false
     }
 }
+
+
+
+
+
+
